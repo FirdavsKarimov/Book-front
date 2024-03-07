33e733cdb9e0305dae8bcb33c51c72a0db67bc17
Firdavs Karimov <mr.firdavs2003.bek@gmail.com>
2024-03-07 23:17:39 +0500

    create HomePage

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import BooksRow from "@/App.vue";
+import PaginationRow from "@/App.vue";
+</script>
+
+<template>
+    <BooksRow />
+    <PaginationRow />
+</template>
+
+<style scoped>
+
+</style>
